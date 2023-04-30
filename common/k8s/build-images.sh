fe8fff120eebfe6b3aecf7d0b4debe65ec2baeee
Juan Sebastián Díaz Osorio <jsdiazo@eafit.edu.co>
2023-04-29 21:44:39 -0500

    back\refac: folder renamed

--- common/k8s/build-images.sh
+++ common/k8s/build-images.sh
@@ -5,7 +5,7 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
 echo "[1] BUILDING CRUD"
-docker build $PWD/apps/web -t juansedo/st1607-crud
+docker build $PWD/apps/jsd-crud -t juansedo/st1607-crud
 docker save juansedo/st1607-crud > st1607-crud.tar
 microk8s ctr image import st1607-crud.tar
 
